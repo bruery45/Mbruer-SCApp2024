bfbe7ff5733806e053e717b12540a027e107eb9a
mbruer <mbruer@student.unimelb.edu.au>
2024-10-19 18:06:59 +1100

    Completed logic for isChild function. Wrote draft for component 1's function

--- sc-take-home-assessment-take-home-2025/folder/get_folder.go
+++ sc-take-home-assessment-take-home-2025/folder/get_folder.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gofrs/uuid"
 
 	"strings"
+
+	"errors"
 )
 
 func GetAllFolders() []Folder {
@@ -24,30 +26,60 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 
 }
 
-func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
+func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
+
+	inOrg := false
+	folderExists := false
+
 	folders := f.folders
 
 	res := []Folder{}
-	for _, f := range folders {
-		if f.OrgId == orgID {
-			res = append(res, f)
+	for _, folder := range folders {
+
+		// checking folder existence for error handling
+		if folder.Name == name {
+			folderExists = true
+
+			if folder.OrgId == orgID {
+				inOrg = true
+			}
+		}
+
+		// Folder must belong to same organisation
+		if folder.OrgId == orgID {
+
+			if isChild(name, folder) {
+				res = append(res, folder)
+			}
 		}
 	}
 
-	return res
+	if !folderExists {
+
+		return nil, errors.New("Error: folder does not exist")
+	}
+
+	if !inOrg {
+		return nil, errors.New("Error: no such folder in specified organisation")
+	}
+
+	return res, nil
 
 	// return []Folder{}
 }
 
-func (f *driver) 
-
-
-func isChild(parentPath string, childCandidate folder) bool {
+func isChild(parentPath string, childCandidate Folder) bool {
 
 	// checking if candidate child is the parent
-	if childCandidate.path == parentPath {
+	if childCandidate.Paths == parentPath {
 		return false
 	}
 
-	else if 
+	// checking if file path includes parent candidate
+	// adding '.' for similarly named file paths
+	if strings.HasPrefix(childCandidate.Paths, parentPath+".") {
+		return true
+	}
+
+	return false
 }
--- sc-take-home-assessment-take-home-2025/main.go
+++ sc-take-home-assessment-take-home-2025/main.go
@@ -22,3 +22,4 @@ func main() {
 
 	folder.PrettyPrint(orgFolder)
 }
+
