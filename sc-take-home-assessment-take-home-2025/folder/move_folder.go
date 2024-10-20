fa5c88eade313c65cbd4d0d3bc9d9d3e61b07973
mbruer <mbruer@student.unimelb.edu.au>
2024-10-20 21:20:53 +1100

    implemented getFolder method within move_folder.go, implemented certain error checks for move_folder

--- sc-take-home-assessment-take-home-2025/folder/move_folder.go
+++ sc-take-home-assessment-take-home-2025/folder/move_folder.go
@@ -1,7 +1,68 @@
 package folder
 
-func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
-	// Your code here...
+import (
+	"errors"
+)
 
-	return []Folder{}, nil
+func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
+
+	/*
+		As the method signature does not contain orgIDs, we assume that only one
+		instance of name and dst occur within the file structure.
+
+		Otherwise, we would implement explicit checking when getting their folders
+	*/
+
+	// checking if moving folder into itself
+	if name == dst {
+
+		return nil, errors.New("Error: Cannot move a folder to itself")
+	}
+
+	// getting desired folders
+	destination, destExists := f.getFolder(dst)
+	source, sourceExists := f.getFolder(name)
+
+	// checking if destination folder exists
+
+	if !destExists {
+		return nil, errors.New("Error: Destination folder does not exist")
+	}
+
+	// checking if source folder exists
+
+	if !sourceExists {
+		return nil, errors.New("Error: Source folder does not exist")
+	}
+
+	// checking if orgIDs match
+
+	if source.OrgId != destination.OrgId {
+
+		return nil, errors.New("Error: Cannot move a folder to a different organization")
+	}
+
+	// folders := f.folders
+
+	// res := []Folder{}
+
+}
+
+func (f *driver) getFolder(name string) (Folder, bool) {
+
+	found := false
+	folders := f.folders
+
+	var folder Folder
+
+	for _, item := range folders {
+
+		//
+		if item.Name == name {
+			found = true
+			folder = item
+		}
+	}
+
+	return folder, found
 }
